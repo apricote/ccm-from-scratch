9c5b48d081e193862958f533350159142e26b256
Julian Tölle <julian.toelle97@gmail.com>
2023-08-23 11:58:59 +0200

    feat: use ProviderID primarily

--- ccm/instancesv2.go
+++ ccm/instancesv2.go
@@ -7,6 +7,8 @@ import (
 	"k8s.io/api/core/v1"
 	cloudprovider "k8s.io/cloud-provider"
 	"k8s.io/klog/v2"
+	"strconv"
+	"strings"
 )
 
 type InstancesV2 struct {
@@ -22,12 +24,9 @@ func (i InstancesV2) InstanceShutdown(ctx context.Context, node *v1.Node) (bool,
 }
 
 func (i InstancesV2) InstanceMetadata(ctx context.Context, node *v1.Node) (*cloudprovider.InstanceMetadata, error) {
-	server, _, err := i.client.Server.GetByName(ctx, node.Name)
+	server, err := i.getServerForNode(ctx, node)
 	if err != nil {
-		return nil, err // TODO: wrap it
-	}
-	if server == nil {
-		return nil, fmt.Errorf("server not found")
+		return nil, err
 	}
 
 	klog.Warningf("Server: %+v", server)
@@ -39,6 +38,39 @@ func (i InstancesV2) InstanceMetadata(ctx context.Context, node *v1.Node) (*clou
 		Zone:          server.Datacenter.Location.Name,
 		Region:        string(server.Datacenter.Location.NetworkZone),
 	}, nil
+}
+
+func (i InstancesV2) getServerForNode(ctx context.Context, node *v1.Node) (*hcloud.Server, error) {
+	var server *hcloud.Server
+
+	if node.Spec.ProviderID != "" {
+		providerID, found := strings.CutPrefix(node.Spec.ProviderID, fmt.Sprintf("%s://", providerName))
+		if !found {
+			return nil, fmt.Errorf("ProviderID does not follow expected format: %s", node.Spec.ProviderID)
+		}
+
+		id, err := strconv.ParseInt(providerID, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse ProviderID to integer: %w", err)
+		}
+
+		server, _, err = i.client.Server.GetByID(ctx, id)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		var err error
+		server, _, err = i.client.Server.GetByName(ctx, node.Name)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if server == nil {
+		return nil, fmt.Errorf("server not found")
+	}
+
+	return server, nil
 }
 
 func getNodeAddresses(server *hcloud.Server) []v1.NodeAddress {
